469ce736b1eaf303fb09413be53b7a05ddb17e29
Niklas Vorberg <services@vorberg-webmail.de>
2024-11-10 15:31:03 +0100

    TASK: added 2.x support

--- init_server.sh
+++ init_server.sh
@@ -2,17 +2,17 @@
 
 # Setting up environment
 if [ -z "$factorio_version" ]; then
-  factorio_version=1.1.109
+    factorio_version=2.0.15
 fi
 
 if [ -z "$factorio_variant" ]; then
-  factorio_variant=headless
+    factorio_variant=headless
 fi
 
 factorio_install=factorio_${factorio_version}_${factorio_variant}.tar.xz
 
 if [ -z "$factorio_save" ]; then
-  factorio_save=savegame
+    factorio_save=savegame
 fi
 
 #########################################################################
@@ -30,7 +30,7 @@ cd downloads
 if test -e "${factorio_install}"; then
     echo "'$factorio_install' file exists."
 else
-    wget -O ${factorio_install} https://factorio.com/get-download/${factorio_version}/${factorio_variant}/linux64
+    wget -O ${factorio_install} https://www.factorio.com/get-download/${factorio_version}/${factorio_variant}/linux64
     wget https://github.com/OpenFactorioServerManager/factorio-server-manager/releases/download/0.10.1/factorio-server-manager-linux-0.10.1.zip
     unzip factorio-server-manager-linux-0.10.1.zip
 fi
@@ -44,7 +44,7 @@ else
 fi
 
 if [[ -d "/serverfiles/factorio-server-manager" ]]; then
-    echo "Directory /serverfiles/factorio-server-managerfound."
+    echo "Directory /serverfiles/factorio-server-manager found."
 else
     mv /serverfiles/downloads/factorio-server-manager /serverfiles/factorio-server-manager
 fi
